afac6dbbaecf3a9d4b7eedcd65acacd55b977f9c
Kesley Rana <77187941+kesrana@users.noreply.github.com>
2025-03-11 11:07:00 -0700

    Time: 0 ms (100%), Space: 17.7 MB (73.01%) - LeetHub

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/2053-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -6,5 +6,18 @@ class Solution:
             return True
         else:
             return False
+
+'''
+first test case: "abcabc" check functionality 
+assertequal(input, expected results)
+
+2nd test case: "aaaaaaa" return true
+
+3rd test case: aaabbbbb return false
+
+4th test case: ""
+
+5th test case: numbers function wouldnt run takes string
+'''
                       
 
