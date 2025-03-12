e9fb66637d61f2a08222fac668147d90b3fa3da9
Kesley Rana <77187941+kesrana@users.noreply.github.com>
2025-03-11 21:01:45 -0700

    Time: 3 ms (82.32%), Space: 19.6 MB (5.31%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,6 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)
         freqMap = Counter(nums)
         result = max(freqMap, key = freqMap.get)
         return result
