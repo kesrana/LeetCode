a754a5d1cda0d48f7c43251f9db7b57eec7bbb2a
Kesley Rana <77187941+kesrana@users.noreply.github.com>
2025-03-14 15:52:33 -0700

    Time: 17 ms (34.02%), Space: 18.6 MB (70.29%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -2,21 +2,25 @@ class Solution:
     def reverseVowels(self, s: str) -> str:
         slist = list(s)
         leftptr = 0
-        rightptr = len(s) - 1
+        rightptr = len(slist) - 1
 
         while leftptr < rightptr:
-            if not self.is_vowel(slist[leftptr]):
+            if self.is_vowel(slist[leftptr]) and self.is_vowel(slist[rightptr]):
+                temp = slist[leftptr]
+                slist[leftptr] = slist[rightptr]
+                slist[rightptr] = temp
                 leftptr += 1
-                continue
-            if not self.is_vowel(slist[rightptr]):
                 rightptr -= 1
-                continue
-            slist[leftptr], slist[rightptr] = slist[rightptr], slist[leftptr]
+            elif self.is_vowel(slist[leftptr]) and not self.is_vowel(slist[rightptr]):
+                rightptr -= 1
+            elif not self.is_vowel(slist[leftptr]) and self.is_vowel(slist[rightptr]):
+                leftptr += 1
+            else:
+                rightptr -= 1
+                leftptr += 1
 
-            leftptr += 1
-            rightptr -= 1
-        return "".join(slist)
-
+        ans = "".join(slist)
+        return ans
 
     def is_vowel(self, char: str) -> bool:
-        return char in "AEIOUaeiou"
+        return True if char in "aeiouAEIOU" else False
