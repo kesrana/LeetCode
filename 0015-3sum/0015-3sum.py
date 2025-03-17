a814e979baa0f3d6cb011630640636683bdd7aca
Kesley Rana <77187941+kesrana@users.noreply.github.com>
2025-03-16 18:53:15 -0700

    Time: 412 ms (94.62%), Space: 20.6 MB (69.09%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,29 +1,43 @@
+#sort the list into ascending order
+#use three loops to find three different values that add up to 0
+# im going make three values into tuple and then add it into list called result
+#0, 1 ,1
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         result = []
-        nums.sort()
+        sortedNum = sorted(nums)
+        for i in range(len(sortedNum)):
+            a = sortedNum[i]
 
-        for i, a in enumerate(nums):
             if a > 0:
                 break
-            if i > 0 and a == nums[i-1]:
+            if i > 0 and sortedNum[i-1] == sortedNum[i]:
                 continue
-            left, right = i+1, len(nums) - 1
+            
+            left, right = i+1, len(sortedNum) - 1
             while left < right:
-                threeSum = a + nums[left] + nums[right]
-                if threeSum < 0:
+                temp = a + sortedNum[left] + sortedNum[right]
+                if temp < 0:
                     left += 1
-                elif threeSum > 0:
+                elif temp > 0:
                     right -= 1
                 else:
-                    result.append([a, nums[left], nums[right]])
+                    result.append([a, sortedNum[left], sortedNum[right]])
                     left += 1
                     right -= 1
-                    while nums[left] == nums[left - 1] and left < right:
+                    while sortedNum[left] == sortedNum[left-1] and left <right:
                         left += 1
-                    while nums[right] == nums[right + 1] and left < right:
+                    while sortedNum[right] == sortedNum[right + 1] and left < right:
                         right -= 1
         return result
+                    
+
+
+
+
+
+                    
+                
         
 
         
