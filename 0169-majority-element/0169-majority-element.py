b11ce9d8933c3d23ea49779b7f1ff537327a6a6d
Kesley Rana <77187941+kesrana@users.noreply.github.com>
2025-10-25 17:04:07 -0700

    Time: 14 ms (16.72%), Space: 19.4 MB (50.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,21 @@
+from collections import defaultdict
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        freqMap = Counter(nums)
-        result = max(freqMap, key = freqMap.get)
-        return result
+        if not nums:
+            return 0
+
+        num_map = defaultdict(int)
+        for i in range(len(nums)):
+            num_map[nums[i]] += 1
+        
+        threshold = len(nums) // 2
+
+        for key, value in num_map.items():
+            if value > threshold:
+                return key
+        
+        
+
         
             
 
