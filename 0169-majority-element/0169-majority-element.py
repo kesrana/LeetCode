9d33631450e795db17a89c20b540d0efd0b8516f
Kesley Rana <77187941+kesrana@users.noreply.github.com>
2025-10-27 15:33:50 -0700

    Time: 2 ms (84.7%), Space: 19.3 MB (50.72%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,24 +1,15 @@
 from collections import defaultdict
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-
+        #define hashmap 
         num_map = defaultdict(int)
+        #store frequency of elements
         for i in range(len(nums)):
             num_map[nums[i]] += 1
         
-        threshold = len(nums) // 2
-
+        #find the most frequent element
         for key, value in num_map.items():
-            if value > threshold:
+            if value > len(nums) // 2:
                 return key
+        return 0
         
-        
-
-        
-            
-
-
-
-            
