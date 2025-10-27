a6d012f12328e37417723a1f6478a79f3c6de371
Kesley Rana <77187941+kesrana@users.noreply.github.com>
2025-10-27 15:02:49 -0700

    Time: 0 ms (100%), Space: 17.8 MB (53.04%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,10 +1,20 @@
+'''
+[3,2,2,3]
+val=3
+k=2
+   v
+[3,2,2,3]
+ ^
+'''
+
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        #last known position where = val
         k = 0
+        #iterate through nums
         for i in range(len(nums)):
+        #only if does not = value
             if nums[i] != val:
                 nums[k] = nums[i]
                 k += 1
         return k
-                
-
