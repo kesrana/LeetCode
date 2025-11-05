faafb2b547f4edc130302d1dda178dd5299a7c56
Kesley Rana <77187941+kesrana@users.noreply.github.com>
2025-11-05 14:21:20 -0800

    Time: 0 ms (100%), Space: 17 MB (91.59%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.c
+++ 0344-reverse-string/0344-reverse-string.c
@@ -1,15 +1,14 @@
 void reverseString(char* s, int sSize) {
-    int first = 0;
-    int last = sSize - 1;
-    
-    while(first < last){
-        int temp = 0;
-        temp = s[first];
-        s[first] = s[last];
-        s[last] = temp;
-        
-        first++;
-        last--;
+    int l = 0;
+    int r = sSize - 1;
+
+    while(l < r){
+        char temp = s[l];
+        s[l] = s[r];
+        s[r] = temp;
+
+        l++;
+        r--;
     }
     
 }
